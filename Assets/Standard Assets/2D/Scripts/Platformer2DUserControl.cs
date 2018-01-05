bd9fa04c5cb30d655f690d759fe05db54385047e
MissingBytes <vidyadheeshadn98@gmail.com>
2018-01-05 22:49:38 +0530

    Contorls for mobile set

---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        public VirtualJoyStickL joyStickL;
+        private PlatformerCharacter2D character;
+        private bool jump;
+         float TimeToJump = 0;
+        [SerializeField] float JumpRate = 2f;
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+             //jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            if (joyStickL.Vertical() > 0.6 && Time.time > TimeToJump)
+            {
+                jump = true;
+                TimeToJump = Time.time + 1 / JumpRate;
+                Debug.Log("Jumping");
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+
+            bool crouch = false; //=Input.GetKey(KeyCode.LeftControl);
+            if (joyStickL.Vertical() < -0.5)
+            {
+                crouch = true;
+            }
+
+            float h = joyStickL.Horizontal();
+            //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            character.Move(h, crouch, jump);
+            jump = false;
+            crouch = false;
+        }
+    }
+}
+
+
---
+++ Assets/Standard Assets/2D/Scripts/VirtualJoyStickL.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class VirtualJoyStickL : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    private Image bgImag;
+    private Image joystickImg;
+    private Vector3 inputVector;
+
+    void Start()
+    {
+        bgImag = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImag.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            //Debug.Log(pos);
+            pos.x = (pos.x / bgImag.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImag.rectTransform.sizeDelta.y);
+            //Debug.Log("a:" + pos);
+            inputVector = new Vector3(pos.x * 2 - 1, 0, pos.y * 2 -1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            joystickImg.rectTransform.anchoredPosition = new Vector3(inputVector.x * (bgImag.rectTransform.sizeDelta.x / 2.5f), inputVector.z * (bgImag.rectTransform.sizeDelta.y / 2.5f));
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+       // Debug.Log("InPointerUp");
+        inputVector = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public float Horizontal()
+    {
+        if (inputVector.x != 0)
+            return inputVector.x;
+        else
+            return Input.GetAxis("Horizontal");
+    }
+
+    public float Vertical()
+    {
+        if (inputVector.z != 0)
+            return inputVector.z;
+        else
+            return Input.GetAxis("Vertical");
+    }
+
+
+
+}
